4d55b34f387c9e86b7cbbaee3908f0336a67605f
+ <+>
2023-08-24 18:40:37 +0300

    still expereincing errors

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,21 @@
-# your code goes here!
+import time
+
+def countdown(number):
+    output = ""
+    while number > 0:
+        output += f'{number} SECOND(S)!\n'
+        number -= 1
+    output += 'HAPPY NEW YEAR!'
+    return output
+
+def countdown_with_sleep(number):
+    output = ""
+    while number > 0:
+        output += f'{number} SECOND(S)!\n'
+        time.sleep(1)
+        number -= 1
+    output += 'HAPPY NEW YEAR!'
+    return output
+
+print(countdown(5))
+print(countdown_with_sleep(5))
--- lib/testing/countdown_test.py
+++ lib/testing/countdown_test.py
@@ -1,10 +1,10 @@
 import io
 import sys
-
+import time
+import unittest
 from countdown import countdown, countdown_with_sleep
 
-class TestCountdown:
-    '''Function countdown() in countdown.py'''
+class TestCountdown(unittest.TestCase):
 
     def test_counts_down_prints_happy_new_year(self):
         '''counts down from number and prints "HAPPY NEW YEAR!"'''
@@ -12,17 +12,17 @@ class TestCountdown:
         sys.stdout = captured_out
         countdown(5)
         sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "5 SECOND(S)!\n4 SECOND(S)!\n" + \
-            "3 SECOND(S)!\n2 SECOND(S)!\n1 SECOND(S)!\nHAPPY NEW YEAR!\n")
+        expected_output = "5 SECOND(S)!\n4 SECOND(S)!\n3 SECOND(S)!\n2 SECOND(S)!\n1 SECOND(S)!\nHAPPY NEW YEAR!"
+        self.assertEqual(captured_out.getvalue().strip(), expected_output)
 
-class TestCountdownWithSleep:
-    '''Function countdown_with_sleep() in countdown.py'''
-
-    def test_counts_down_prints_happy_new_year(self):
-        '''counts down from number and prints "HAPPY NEW YEAR!"'''
+    def test_counts_down_prints_happy_new_year_with_sleep(self):
+        '''counts down from number and prints "HAPPY NEW YEAR!" with sleep'''
         captured_out = io.StringIO()
         sys.stdout = captured_out
         countdown_with_sleep(5)
         sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "5 SECOND(S)!\n4 SECOND(S)!\n" + \
-            "3 SECOND(S)!\n2 SECOND(S)!\n1 SECOND(S)!\nHAPPY NEW YEAR!\n")
+        expected_output = "5 SECOND(S)!\n4 SECOND(S)!\n3 SECOND(S)!\n2 SECOND(S)!\n1 SECOND(S)!\nHAPPY NEW YEAR!"
+        self.assertEqual(captured_out.getvalue().strip(), expected_output)
+
+if __name__ == '__main__':
+    unittest.main()
